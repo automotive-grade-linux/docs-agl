a0ce506d8b13f0576ec8c811487cc17a45128b6b
Leon Anavi <leon.anavi@konsulko.com>
2016-10-28 18:46:15 +0300

    agldoc.sh: Add option to export all documents
    By default agldoc.sh exports documents for all
    supported machines to the specified format. By
    default the format is HTML.
    
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

--- agldoc.sh
+++ agldoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### default options
-MACHINE=qemu
+MACHINE=all
 DOCTYPE=html
 HELP=0
 
@@ -19,7 +19,7 @@ done
 if [ 1 == $HELP ] ; then
 	printf "Usage: . agldoc.sh [options]\n"
 	printf "Options:\n"
-	printf "\t-m|--machine <machine>\n\t\twhat machine to use\n\t\tdefault: 'qemu'\n"
+	printf "\t-m|--machine <machine>\n\t\twhat machine to use\n\t\tdefault: 'all'\n"
 	printf "\t-d|--document <pdf|html>\n\t\twhat document format to use\n\t\tdefault: 'html'\n"
 	printf "\t-h|--help\n\t\tget some help\n"
 	exit 0
@@ -31,18 +31,11 @@ case $MACHINE in
 	qemux86-64|qemux86) MACHINE=qemu;;
 esac
 
-FILEDOC="../source-code/$MACHINE.md"
-
 DIRROOT=$(dirname "$0")
 cd $DIRROOT
 mkdir -p export
 cp -R templates export
 cd export
-
-if [ ! -e $FILEDOC ] ; then
-	echo "Document for $MACHINE not found."
-	exit 1
-fi
 
 if [ "pdf" == $DOCTYPE ] ; then
 	FILECONFIG="-N --template=templates/pdf/agl.tex --variable mainfont=\"Arial\" --variable sansfont=\"Arial\" --variable monofont=\"Arial\" --variable fontsize=12pt --latex-engine=xelatex --toc"
@@ -54,8 +47,25 @@ else
 fi
 
 mkdir -p "$DOCTYPE"
-FILEEXPORT="$DOCTYPE/$MACHINE.$DOCTYPE"
 
-pandoc ../source-code/source-code.md $FILEDOC ../source-code/troubleshooting.md $FILECONFIG -o $FILEEXPORT
-
-echo "Document exported to $DIRROOT/export/$FILEEXPORT"
+if [ "all" == $MACHINE ] ; then
+	echo "Exporting documentation for all machines."
+	for TARGET in ../source-code/machines/*.md
+	do
+		TARGET=$(basename $TARGET)
+		TARGET=${TARGET%.*}
+		FILEEXPORT="$DOCTYPE/$TARGET.$DOCTYPE"
+		FILEDOC="../source-code/machines/$TARGET.md"
+		pandoc ../source-code/source-code.md $FILEDOC ../source-code/troubleshooting.md $FILECONFIG -o $FILEEXPORT
+		echo "Document exported to $DIRROOT/export/$FILEEXPORT"
+	done
+else
+	FILEDOC="../source-code/machines/$MACHINE.md"
+	if [ ! -e $FILEDOC ] ; then
+		echo "Document for $MACHINE not found."
+		exit 1
+	fi
+	FILEEXPORT="$DOCTYPE/$MACHINE.$DOCTYPE"
+	pandoc ../source-code/source-code.md $FILEDOC ../source-code/troubleshooting.md $FILECONFIG -o $FILEEXPORT
+	echo "Document exported to $DIRROOT/export/$FILEEXPORT"
+fi
